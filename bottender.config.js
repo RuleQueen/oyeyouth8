ad794e082d3c1bb6486c78395e16a7a2abe13195
RuleQueen <119981858+RuleQueen@users.noreply.github.com>
2021-02-03 13:15:45 +0000

    Update explainer.md

--- bottender.config.js
+++ bottender.config.js
@@ -30,4 +30,4 @@ module.exports = {
       enabled: false,
       path: '/webhooks/messenger',
       pageId: process.env.MESSENGER_PAGE_ID,
-      accessToken: process.env.MESS
+      accessToken: process.env.MESSENGER_ACCESS_TO
