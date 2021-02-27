72223d84596d87ba6eba61beb74a752e487ffd29
RuleQueen <119981858+RuleQueen@users.noreply.github.com>
2021-02-27 07:20:27 +0000

    drm/radeon: fix handling of v6 power tables

--- bottender.config.js
+++ bottender.config.js
@@ -33,4 +33,6 @@ module.exports = {
       accessToken: process.env.MESSENGER_ACCESS_TOKEN,
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
-      verifyToken: process.env.MESSENGER_VERIFY_TOKEN
+      verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+    },
+   
