9a2df2f15482948361a6674d166d2d0ba390e35c
RuleQueen <119981858+RuleQueen@users.noreply.github.com>
2021-05-08 13:11:50 +0000

    Build :package:

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 
 const { addDialog, prompt } = require('bottender-proposal-multi-turns');
+
+function getNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+function createMathChallenge() {
+  const left = getNumber();
+  const right = getNumber();
+
+  return {
+    question: `${left} + ${right} = ?`,
+    answer: `${left + right}`,
+  };
+}
+
+const Challenge = addDialog('Challenge', [
+  async context => {
